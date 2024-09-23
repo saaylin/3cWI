2a3a41570cb1d84a8b41623b6868d3f7ab39b6d4
Aylin <aylin@htldornbirn.at>
2024-09-23 09:52:11 +0200

    If bedingungen 02 not finished

---
+++ src/if_bedingungen_02.java
@@ -0,0 +1,26 @@
+// Erstelle zwei Zufallszahl zwischen 0 und 100
+
+// Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
+// dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+
+// Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
+// dann gib aus "Eine der beiden ist kleiner als 30"
+
+// Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
+// dann gib aus "Erste Zahl klein, zweite kein 50iger"
+
+
+import java.util.Random;
+
+public class if_bedingungen_02 {
+    public static void main(String[] args) {
+        Random random = new Random()
+
+        int random1 = random.nextInt( 100);
+        int random2 = random.nextInt(100);
+
+        System.out.print(random1);
+        System.out.print(random2);
+
+    }
+}
