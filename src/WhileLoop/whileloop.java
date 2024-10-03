5dead480fa0a865b50877b4353d536111fc30f89
Aylin <aylin@htldornbirn.at>
2024-10-03 09:10:11 +0200

    WhileLoop Aufgabe 01 finished

---
+++ src/WhileLoop/whileloop.java
@@ -0,0 +1,25 @@
+package WhileLoop;
+
+import java.util.Random;
+
+import static com.sun.beans.introspect.PropertyInfo.Name.bound;
+
+public class whileloop {
+    public static void main(String[] args){
+
+        Random random = new Random();
+        boolean isFinished = false;
+
+    while (isFinished) {
+        int randomValue = random.nextInt(bound 100);
+        System.out.print("Hello" + randomValue);
+
+        if (randomValue > 90){
+            isFInished-true;
+        }
+    }
+
+    System.out.print("finished");
+    }
+
+}
---
+++ src/WhileLoop/whileloop01.java
@@ -0,0 +1,28 @@
+//Erstelle ein Programm, dass Zufallszahlen zwischen 10 und 30 generiert.
+// Das Programm soll die Zufallszahlen zusammenzählen. Sobald die Zahl 15 oder die Zahl 25 kommt,
+// wird das Programm beendet und die Summe der vorherigen Zufallszahlen ausgegeben!
+
+package WhileLoop;
+
+import java.util.Random;
+
+public class whileloop01 {
+    public static void main(String[] args) {
+        Random random = new Random ();
+        int sum = 0;
+        int number;
+
+        while(true){
+            number = 10+ random.nextInt(21);
+
+        if (number == 15 || number 25) {
+            break;
+            }
+
+        sum += number;
+        System.out.println("Generierte Zahl: " + number);
+        }
+
+        System.out.println("Die Summe der vorherigen Zufallszahlen beträgt: " +sum);
+    }
+}
