4519b07650d71dd5c003db7146e00150a6d18685
Aylin <aylin@htldornbirn.at>
2024-10-17 09:26:13 +0200

    Caeser FINISHED

---
+++ src/Strings/caeser.java
@@ -0,0 +1,26 @@
+/*Bei der Cäsar-Verschlüsselung werden die Buchstaben
+um eine fixe Anzahl Stellen verschoben (z.B. 2)
+a wird zu c, f wird zu h, z wird zu b usw.
+
+Implementiere eine Methode encrypt(String):String
+und eine Methode decrypt(String):String */
+
+package Strings;
+
+public class caeser {
+
+    public static void main(String[] args) {
+        String result = encrypt("abc");
+        System.out.println(result);
+        // Expected cde
+    }
+
+    public static String encrypt (String data ){
+        return"";
+    }
+
+    public static String decrypt (String data){
+        return"";
+    }
+}
+
