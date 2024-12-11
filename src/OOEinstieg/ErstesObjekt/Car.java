37990be207ce607582ed5bc67be5d109829bc4c0
Aylin <aylin@htldornbirn.at>
2024-12-11 08:01:05 +0100

    Objekt01 04 FINISHED

---
+++ src/OOEinstieg/ErstesObjekt/Car.java
@@ -0,0 +1,35 @@
+package OOEinstieg.ErstesObjekt;
+
+public class Car {
+    // instanz / gedächnisvariablen
+
+    //don't do that later
+    public int fuelConsumption;
+    public String brand;
+    public String serialNumber;
+    public int fuelAmount;
+    public String bremse;
+    public int turboBoost;
+    public int amountOfRepetition;
+
+
+    public void drive() {
+        this.fuelAmount = this.fuelAmount
+                - fuelConsumption;
+        System.out.println("ich fahre");
+    }
+
+    public void doBreak(){
+        System.out.println("ich bremse"); 
+    }
+
+    public void turboBoost(){
+        if (fuelAmount > fuelConsumption * 0.1){
+            System.out.println("SuperBoostMode");
+        } else {
+            System.out.println("Not enough fuel to go Super fast!");
+        }
+    }
+
+
+}
--- src/OOEinstieg/ErstesObjekt/Objekt01.java
+++ src/OOEinstieg/ErstesObjekt/Objekt01.java
@@ -9,14 +9,19 @@ public class Objekt01 {
         c1.brand = "BMW";
         c1.fuelConsumption = 7;
         c1.serialNumber = "A1234";
+        c1.doBreak();
+        c1.fuelAmount = 80;
+
 
         Car c2 = new Car();
         c2.brand = "Porsche";
         c2.fuelConsumption = 6;
         c2.serialNumber = "A5678";
+        c2.bremse = "ich bremse";
 
-        System.out.println(c1.serialNumber);
-        System.out.println(c2.serialNumber);
+        System.out.println(c1.fuelAmount);
+        c1.drive();
+        System.out.println(c2.bremse);
 
 
         // D R Y
