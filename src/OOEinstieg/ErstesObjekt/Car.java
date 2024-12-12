43fa9fa9839a2aad7348ab872ca9a11c1345d2a7
Aylin <aylin@htldornbirn.at>
2024-12-12 08:53:58 +0100

    Objekt01 04.2 FINISHED

--- src/OOEinstieg/ErstesObjekt/Car.java
+++ src/OOEinstieg/ErstesObjekt/Car.java
@@ -4,22 +4,22 @@ public class Car {
     // instanz / gedächnisvariablen
 
     //don't do that later
-    public int fuelConsumption;
-    public String brand;
-    public String serialNumber;
-    public int fuelAmount;
-    public String bremse;
-    public int turboBoost;
-    public int amountOfRepetition;
+    private int fuelConsumption;
+    private int Tank;
+    private int fuelAmount;
+    private String brand;
+    private String serialNumber;
+    private String color;
 
 
     public void drive() {
-        this.fuelAmount = this.fuelAmount
-                - fuelConsumption;
-        System.out.println("ich fahre");
+        if (fuelAmount >= fuelConsumption) {
+            this.fuelAmount = this.fuelAmount - fuelConsumption;
+            System.out.println("ich fahre");
+        }
     }
 
-    public void doBreak(){
+    public void Break() {
         System.out.println("ich bremse"); 
     }
 
@@ -31,6 +31,78 @@ public class Car {
         }
     }
 
+    public void honk(int Repetitions){
+        while (Repetitions > 0){
+            Repetitions -= 1;
+            System.out.println("tuut");
+        }
+    }
 
+    public void getRemainingRange(){
+        int range = fuelAmount / fuelConsumption;
+
+        if (range > 1){
+            System.out.printf("you can drive more kilometers!", range);
+        }
+        if (range == 1){
+            System.out.printf("you can drive more kilometers!", range);
+        }
+        if (range < 1){
+            System.out.printf("you can't drive any more kilometers!");
+        }
+    }
+
+    public void setTank(int tank) {
+        Tank = tank;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setFuelAmount(int fuelAmount) {
+        if (fuelAmount>100){
+            this.fuelAmount=100;
+        } else {
+            this.fuelAmount = fuelAmount;
+        }
+
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public int getTank() {
+        return Tank;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 }
-// to push
--- src/OOEinstieg/ErstesObjekt/Objekt01.java
+++ src/OOEinstieg/ErstesObjekt/Objekt01.java
@@ -3,28 +3,18 @@ package OOEinstieg.ErstesObjekt;
 
 public class Objekt01 {
     public static void main(String[] args) {
-        int a = 7;
 
         Car c1 = new Car();
-        c1.brand = "BMW";
-        c1.fuelConsumption = 7;
-        c1.serialNumber = "A1234";
-        c1.doBreak();
-        c1.fuelAmount = 80;
-
-
-        Car c2 = new Car();
-        c2.brand = "Porsche";
-        c2.fuelConsumption = 6;
-        c2.serialNumber = "A5678";
-        c2.bremse = "ich bremse";
+        c1.setBrand("Brand");
+        c1.setFuelConsumption(7);
+        c1.setSerialNumber("1234");
+        c1.setFuelAmount(800);
+        c1.setTank(70);
 
         System.out.println(c1.fuelAmount);
         c1.drive();
-        System.out.println(c2.bremse);
-
-
-        // D R Y
+        System.out.println(c1.fuelAmount);
+        c1.turboBoost();
     }
 }
 
